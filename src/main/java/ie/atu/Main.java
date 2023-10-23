e24a5602d535652218b8695662970b333fae6677
Aidan Malone <g00423306@atu.ie>
2023-10-23 10:28:25 +0100

    Added a display info and get userinputs

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.printf("Hello and welcome!");
+
         Person student1 = new Person("Aidan", "Malone", 20);
-        System.out.println("Details are" + " " + student1.getFirstName());
+        student1.displayInfo();
     }
 }
