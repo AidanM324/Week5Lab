cb442d992be07600fc35b5a78a179180891e2119
Aidan Malone <g00423306@atu.ie>
2024-10-31 18:32:21 +0000

    Added put and delete mapping to the productcontroller

--- src/main/java/ie/atu/week5/ProductController.java
+++ src/main/java/ie/atu/week5/ProductController.java
@@ -3,10 +3,7 @@ package ie.atu.week5;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -34,6 +31,27 @@ public class ProductController {
     public ResponseEntity<List> addProduct(@RequestBody Product product)
     {
         products.add(product);
+        return ResponseEntity.ok(products);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<List> editProduct(@PathVariable int id, @RequestBody Product product)
+    {
+        for(int i = 0; i< products.size(); i++){
+            if(products.get(i).getId() == id){
+                products.set(i, product);
+            }
+        }
+        return ResponseEntity.ok(products);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<List> deleteProduct(@PathVariable int id) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getId() == id) {
+                products.remove(id);
+            }
+        }
         return ResponseEntity.ok(products);
     }
 }
